a151dc1427972bf594764accc4a0496e130bf8eb
kunalherkal <kunalrherkal@gmail.com>
2015-12-01 21:55:09 +0530

    Added anti-clockwise movement for orange.

--- src/main/java/com/kunalherkal/rubikscube/sides/Orange.java
+++ src/main/java/com/kunalherkal/rubikscube/sides/Orange.java
@@ -46,25 +46,25 @@ public final class Orange extends SideAdapter {
 		super.rotateAntiClockwise();
 
 		Color[] tempSideColors = new Color[3];
-		tempSideColors[0] = cube.getBlue().getColors()[6];
-		tempSideColors[1] = cube.getBlue().getColors()[3];
-		tempSideColors[2] = cube.getBlue().getColors()[0];
+		tempSideColors[0] = cube.getYellow().getColors()[0];
+		tempSideColors[1] = cube.getYellow().getColors()[1];
+		tempSideColors[2] = cube.getYellow().getColors()[2];
+		
+		cube.getYellow().getColors()[0] = cube.getBlue().getColors()[6];
+		cube.getYellow().getColors()[1] = cube.getBlue().getColors()[3];
+		cube.getYellow().getColors()[2] = cube.getBlue().getColors()[0];
+		
+		cube.getBlue().getColors()[0] = cube.getWhite().getColors()[6];
+		cube.getBlue().getColors()[3] = cube.getWhite().getColors()[7];
+		cube.getBlue().getColors()[6] = cube.getWhite().getColors()[8];
+		
+		cube.getWhite().getColors()[6] = cube.getGreen().getColors()[8];
+		cube.getWhite().getColors()[7] = cube.getGreen().getColors()[5];
+		cube.getWhite().getColors()[8] = cube.getGreen().getColors()[2];
 
-		cube.getBlue().getColors()[6] = cube.getWhite().getColors()[6];
-		cube.getBlue().getColors()[3] = cube.getWhite().getColors()[3];
-		cube.getBlue().getColors()[0] = cube.getWhite().getColors()[0];
-
-		cube.getWhite().getColors()[0] = cube.getGreen().getColors()[0];
-		cube.getWhite().getColors()[3] = cube.getGreen().getColors()[3];
-		cube.getWhite().getColors()[6] = cube.getGreen().getColors()[6];
-
-		cube.getGreen().getColors()[0] = cube.getYellow().getColors()[8];
-		cube.getGreen().getColors()[3] = cube.getYellow().getColors()[5];
-		cube.getGreen().getColors()[6] = cube.getYellow().getColors()[2];
-
-		cube.getYellow().getColors()[2] = tempSideColors[0];
-		cube.getYellow().getColors()[5] = tempSideColors[1];
-		cube.getYellow().getColors()[8] = tempSideColors[2];
+		cube.getGreen().getColors()[2] = tempSideColors[0];
+		cube.getGreen().getColors()[5]= tempSideColors[1];
+		cube.getGreen().getColors()[8] = tempSideColors[2];
 	}
 
 }
